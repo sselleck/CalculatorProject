bfa36a6c36b3bfbab389434a315f1e17fc53e813
Samantha Selleck <samanthaselleck@Samanthas-MacBook-Pro.local>
2017-02-13 13:58:01 -0500

    Calc. Playground

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -2,24 +2,69 @@
 //  ViewController.swift
 //  calculator
 //
-//  Created by Samantha Selleck on 1/28/17.
+//  Created by Samantha Selleck and Alex Wise on 1/28/17.
 //  Copyright © 2017 S. Selleck. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet var solutionLabel: UILabel! //variable for the solution output
+    @IBOutlet var operators: UILabel!  //variable for all the operators
+    @IBOutlet var numbers: UILabel!  //var's for the numbers
+    @IBOutlet var leftP: UILabel! //var for the open par. (
+    @IBOutlet var rightP: UILabel! //var for the close par. )
+    @IBOutlet var clear: UILabel! //var for the clear button
+    
+    
+    @IBAction func displayOutput(_ sender: UIButton) {
+        
+        
+    
+    }
+    
+    func add(_ a: Double,_ b: Double) ->Double // function to add two doubles
+    {
+        return a+b
+    }
+    //function to subtract two double
+    func sub(_ a: Double,_ b: Double)->Double
+    {
+        return a-b
+    }
+    //function to multiple two double
+    func mult(_ a: Double,_ b: Double) ->Double
+    {
+        return a*b
+    }
+    //function to divide two doubles
+    func div(_ a: Double,_ b: Double) ->Double
+    {
+        return a/b
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //Function that will calculate the expression
+    func doMath(_ a: Double,_ b: Double,_ op:Character)->Double
+    {
+        var answer: Double = 0
+        if op == "+" {
+            answer = add(a,b)
+        }
+        else if op == "-"{
+            answer = sub(a,b)
+        }
+        else if op == "*"{
+            answer = mult(a,b)
+        }
+        else if op == "/"{
+            answer = div(a,b)
+        }
+        return answer;
     }
 
-
+    
+    
+   
 }
 
